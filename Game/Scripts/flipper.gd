bc32ed68c68072b3e95604db520a1456b42be8d8
Jeff-MM <jeffrey.mumma@washburn.edu>
2022-09-10 12:53:40 -0500

    Patched flippers

--- Game/Scripts/flipper.gd
+++ Game/Scripts/flipper.gd
@@ -1,32 +1,15 @@
-extends Node
+extends KinematicBody2D
 
-onready var flipper_right = $"Flipper"
-onready var flipper_left = $"Flipper2"
-var min_right = 0
-var min_left = 180
-var max_right = 90
-var max_left = 90
-var right_deg = 0
-var left_deg = 0
-var speed = 400
-func _ready():
-	flipper_right.rotation_degrees=right_deg
-	flipper_left.rotation_degrees=left_deg
+onready var right_flipper = $"Flipper"
+onready var left_flipper = $"Flipper2"
+export var action:String="left_flipper"
+export var min_deg = 0
+export var max_deg = 90
+export var speed = 900
 
 func _process(delta):
-#right flipper script
-	if(Input.is_action_pressed("right_flipper")&&right_deg<max_right):
-		right_deg+=delta*speed
-		flipper_right.rotation_degrees=right_deg
-	elif(right_deg>min_right):
-		right_deg-=delta*speed
-		flipper_right.rotation_degrees=right_deg
-
-#left flipper script
-	if(Input.is_action_pressed("left_flipper")&&left_deg>max_left):
-		left_deg-=delta*speed
-		flipper_left.rotation_degrees=left_deg
-	elif(left_deg<min_left):
-		left_deg+=delta*speed
-		flipper_left.rotation_degrees=left_deg
-
+	if(Input.is_action_pressed(action)):
+		rotation_degrees+=delta*speed
+	else:
+		rotation_degrees-=delta*speed
+	rotation_degrees=clamp(rotation_degrees,min_deg,max_deg)
