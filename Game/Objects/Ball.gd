20690561e1044831a88235f5efcd99bd3ad1b1cb
thexerothermic <elijahbrian0@gmail.com>
2022-09-09 20:25:30 -0500

    First commit for ball chute

--- Game/Objects/Ball.gd
+++ Game/Objects/Ball.gd
@@ -2,13 +2,13 @@ extends RigidBody2D
 
 
 var myTimer:Timer
-
+var reset = false
+const is_ball = true
 func _ready():
 	myTimer=Timer.new()                #initializes a copy of a class (an instance)
 	add_child(myTimer)                                            #Needs to be added to scene to not be collected as garbage
 	myTimer.connect("timeout",self,"challenge")                #Connects the signal to a custom function
-	reset_timer()                        #Called once to get the timer going
-
+	#reset_timer()        #Called once to get the timer going
 
 	self.connect("body_entered",self,"bumped")
 func reset_timer():
@@ -23,3 +23,9 @@ func bumped(body:Node):
 	#Called when the ball hits an object. The ball is in charge of creating bumped requests on other objects
 	if(body.has_method("bumped")):
 		body.bumped(self)
+		
+func _integrate_forces(state):
+	state.linear_velocity = state.linear_velocity
+	if(reset):
+		self.global_position = Vector2(400,400)
+		reset = false
