0ce334a30a507f515b6f06e2fc5640f135f3efce
KyleKleinschmidt <112215427+KyleKleinschmidt@users.noreply.github.com>
2022-09-16 19:20:02 -0500

    flipper sfx
    added flipper sfx

--- Game/Scripts/flipper.gd
+++ Game/Scripts/flipper.gd
@@ -4,10 +4,15 @@ export var action:String="left_flipper"
 export var min_deg = 0
 export var max_deg = 90
 export var speed = 900
+var play_sfx = true
 
 func _process(delta):
 	if(Input.is_action_pressed(action)):
 		rotation_degrees+=delta*speed
+		if(play_sfx):
+			$flipper_sound.play()
+			play_sfx = false
 	else:
 		rotation_degrees-=delta*speed
+		play_sfx = true
 	rotation_degrees=clamp(rotation_degrees,min_deg,max_deg)
