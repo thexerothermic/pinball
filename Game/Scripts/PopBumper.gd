f574955562c23188b2b11182c95cf091fc671a61
thexerothermic <elijahbrian0@gmail.com>
2022-09-26 00:58:18 -0500

    Added basic full frame, pop bumpers

---
+++ Game/Scripts/PopBumper.gd
@@ -0,0 +1,11 @@
+extends RigidBody2D
+
+func bumped(body:Node):
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	#Called by the ball when this object is touched by the ball
+	if(body is RigidBody2D):
+		#If it's a ball...
+		var randomXVelocity = rng.randf_range(300, 400)
+		var randomYVelocity = rng.randf_range(300, 400)
+		body.linear_velocity+=((body.global_position-self.global_position)+Vector2(randomXVelocity,randomYVelocity)).normalized()*900
