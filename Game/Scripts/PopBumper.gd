7f9fe3640c0e8e0daf213fd5db67dd7f830a1eba
thexerothermic <elijahbrian0@gmail.com>
2022-10-10 00:25:12 -0500

    Added Pop Bumper Animation and SFX

--- Game/Scripts/PopBumper.gd
+++ Game/Scripts/PopBumper.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
-
+onready var _animated_sprite = $AnimatedSprite
+const points = 50
 func bumped(body:Node):
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
@@ -10,3 +11,13 @@ func bumped(body:Node):
 		var randomYVelocity = rng.randf_range(300, 400)
 		body.linear_velocity+=((body.global_position-self.global_position)+Vector2(randomXVelocity,randomYVelocity)).normalized()*900
 		get_parent().get_node("UI").addPoints(100)
+		_animated_sprite.play("activate")
+		SoundSystem.play_sound("pop_bumper_hit")
+#returns how many points this target is worth for upgrade calculation
+func get_points():
+	return points
+
+
+func _on_AnimatedSprite_animation_finished():
+	_animated_sprite.stop()
+	_animated_sprite.play("default")
