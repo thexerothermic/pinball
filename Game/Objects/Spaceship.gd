b5dd4144f14cb64e0bb4bb8c90d1208930754271
thexerothermic <elijahbrian0@gmail.com>
2022-11-13 19:59:08 -0600

    Ongoing work for spaceship

--- Game/Objects/Spaceship.gd
+++ Game/Objects/Spaceship.gd
@@ -1,8 +1,8 @@
 extends RigidBody2D
-
-const wiggleDuration = 0.25
+onready var _animated_sprite = $AnimatedSprite
+const wiggle_duration = 0.25
 var initial_position = self.global_position
-
+var health = 5
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -10,8 +10,7 @@ var initial_position = self.global_position
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	_animated_sprite.play("enter")
 func bumped(body:Node):
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
@@ -22,22 +21,33 @@ func bumped(body:Node):
 		var randomYVelocity = rng.randf_range(1200, 1600)
 		body.linear_velocity+=((body.global_position-self.global_position)+Vector2(randomXVelocity,randomYVelocity)).normalized()*900
 		
-		
+		#play sound
 		SoundSystem.play_sound("bump1")
-
-		SoundSystem.play_sound("pop_bumper_hit")
-		#play hit animation and make ship wiggle a little
+		
 		#decrement health
-		#if health == 0, destroy ship func
-#on signal emitted, place spaceships in scene: will use true bool for now
-	#play entrance animation
-	#play base animation
-#destroy ship func
-	#play destruction animation
-	#remove node from scene
-	#cause new ball to enter scene or update a singleton
-func _integrate_forces(state):
-	print("hj")
-	#move in direction oppsite of where ball hit with certain duration and speed
+		health = health - 1
+		
+		#if health == 0, destroy ship
+		if (health == 0):
+			#play destroy animation
+			_animated_sprite.play("destroy")
+			
+			#either change singleton or drop ball
+			
+			#remove ship from scene
+			queue_free()
+			
+		#if spaceship was not destroyed
+		#play hit animation  - wiggle may be added later
+		_animated_sprite.play("on_hit")
+			
+		
+func _on_AnimatedSprite_animation_finished():
+	if(_animated_sprite.animation == "enter"):
+		self.get_node("CollisionPolygon2D").disabled = false
+	if (_animated_sprite.animation == "on_hit"):	
+		_animated_sprite.stop()
+		_animated_sprite.play("default")
+
 	
 
