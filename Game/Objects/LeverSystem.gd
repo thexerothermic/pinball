b46aa78aaed5986d49f11bd5b87cd6956faf8e90
KyleKleinschmidt <112215427+KyleKleinschmidt@users.noreply.github.com>
2022-11-21 10:49:14 -0600

    added lever arrow
    added arrow to the lever for better visibility

--- Game/Objects/LeverSystem.gd
+++ Game/Objects/LeverSystem.gd
@@ -12,16 +12,20 @@ func _ready():
 	connect("body_entered",self,"ball_entered")
 	var game = get_owner()
 	game.connect("reset_lever", self, "on_reset_lever")
+	$LeverArrow.turn_on()
+	
 func ball_entered(ball_ref):
 	
 	switch()
 	SoundSystem.play_sound(lever_sound)
+	
 
 func switch():
 	if(up):
 		get_node("AnimationPlayer").play("Down")
 		up=false
 		emit_signal("lever_down")
+		$LeverArrow.turn_off()
 	#elif(!up):
 		#get_node("AnimationPlayer").play("Up")
 		#up=true
@@ -31,3 +35,4 @@ func on_reset_lever():
 	print("signal emitted")
 	get_node("AnimationPlayer").play("Up")
 	up=true
+	$LeverArrow.turn_on()
