6d909b4ceeaa4a32cc702eeff7fe16b3e0c20aa6
KyleKleinschmidt <112215427+KyleKleinschmidt@users.noreply.github.com>
2022-11-21 11:04:44 -0600

    added point values
    added point values to:
    -launchers
    -worm

--- Game/Scripts/WormholeIn.gd
+++ Game/Scripts/WormholeIn.gd
@@ -6,6 +6,7 @@ extends RigidBody2D
 # var b = "text"
 
 var effect=preload("res://Game/Objects/WarpEffect.tscn")
+var points=150
 
 func bumped(body:Node):
 	if(body is RigidBody2D):		#If it's a ball...
@@ -14,6 +15,7 @@ func bumped(body:Node):
 	var x=effect.instance()
 	get_parent().add_child(x)
 	x.position=get_node("Node2D").global_position
+	get_parent().get_node("UI").addPoints(points)
 #	add_child(effect.instance())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
