ff6f50790dc947f1d6b15d6fcdaa080f11b38cc6
KyleKleinschmidt <112215427+KyleKleinschmidt@users.noreply.github.com>
2022-11-21 11:15:36 -0600

    fixed conflicted file
    fixed conflicted file

--- Game/Scripts/WormholeIn.gd
+++ Game/Scripts/WormholeIn.gd
@@ -1,12 +1,11 @@
 extends RigidBody2D
-
+const points = 100
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 var effect=preload("res://Game/Objects/WarpEffect.tscn")
-var points=150
 
 func bumped(body:Node):
 	if(body is RigidBody2D):		#If it's a ball...
@@ -15,9 +14,7 @@ func bumped(body:Node):
 	var x=effect.instance()
 	get_parent().add_child(x)
 	x.position=get_node("Node2D").global_position
-	get_parent().get_node("UI").addPoints(points)
-#	add_child(effect.instance())
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	get_parent().get_node("UI").addPoints(100)
+#	
+func get_points():
+	return points
